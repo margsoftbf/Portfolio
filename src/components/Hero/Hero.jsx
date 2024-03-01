5d1d6687377590761cb984a72caddf5e2ab60dda
Marcin <mgarski@tutanota.com>
2024-02-29 19:52:59 -0500

    Add admin project

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
 			animate={{ opacity: 1 }}
 			transition={{ duration: 1, ease: [0.6, -0.05, 0.01, 0.99] }}
 		>
-			<div className='absolute flex justify-center top-12 right-0 bottom-0 left-0 w-full h-full lg:justify-end lg:mt-8'>
+			<div className='absolute flex justify-center top-12 right-0 bottom-0 left-0 w-full h-full lg:justify-end'>
 				<HeroBgAnimation />
 			</div>
 			<div className='relative flex justify-between flex-col items-center w-full lg:flex-row'>
@@ -50,7 +50,7 @@ const Hero = () => {
 						</span>
 					</div>
 					<p className='leading-7 text-stone-400 lg:text-left md:leading-8'>
-					Currently I am in the process of moving to Poland after 9 years spent in the United States. Analytical and detail-oriented professional with valuable experience in front-end development, project management and web design across various industries. Highly skilled in building and implementing high-quality, scalable and maintainable components for web applications while translating design mock-ups into functional features and reviewing codes. Adept at debugging software defects, resolving issues and providing constructive feedback. Capable of gathering user input to create functional, user-friendly webpages. Track record of keeping up with the latest industry trends, technologies, frameworks and best practices in front-end development.
+					Currently I am in the process of moving to Poland after 9 years spent in the United States. Analytical and detail-oriented professional with valuable experience in front-end development, project management and web design across various industries. Highly skilled in building and implementing high-quality, scalable and maintainable components for web applications while translating design mock-ups into functional features and reviewing codes. Track record of keeping up with the latest industry trends, technologies, frameworks and best practices in front-end development.
 					</p>
 					<a
 						href='./assets/Resume/Marcin-Garski-Resume.pdf'
